f22700000093cbe9f134e628f24681374c3e32be
rifqirafialdy <rifqirafialdy@gmail.com>
2023-04-12 03:34:47 +0700

    upload image

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -2,28 +2,34 @@
 const { db, query } = require('../database')
 module.exports = {
     addProduct: async (req, res) => {
-        const { name, description, price,category } = req.body
+        const { name, description, price, category } = req.body
         const productQuery = await query(`SELECT * FROM products WHERE name = ${db.escape(name)}`)
         const categoryQuery = await query(`SELECT * FROM categories WHERE category_name=${db.escape(category)}`)
         const categoryId = categoryQuery[0].categories_ID
         console.log(categoryId);
+        console.log(req.file);
         if (productQuery.length > 0) {
-            return res.status(200).send({message:'name duplicate'})
+            return res.status(200).send({ message: 'name duplicate' })
             
         }
-        const addProduct = await query(`INSERT INTO products VALUES (null,${db.escape(name)},${db.escape(description)},null,${db.escape(price)},false,null,${db.escape(categoryId)})`)
-        return res.status(200).send({message:'Register Succes'})
+        let filename = null
+        if (req.file) {
+            filename ='/' + req.file.filename
+            
+        }
+        const addProduct = await query(`INSERT INTO products VALUES (null,${db.escape(name)},${db.escape(description)},${db.escape(filename)},${db.escape(price)},false,null,${db.escape(categoryId)})`)
+        return res.status(200).send({ message: 'Register Succes' })
     },
     addCategory: async (req, res) => {
         const name = req.body.name
         const categoryQuery = await query(`SELECT * FROM categories WHERE category_name=${db.escape(name)}`)
         console.log(categoryQuery);
         if (categoryQuery.length > 0) {
-            return res.status(200).send({message:'category already use'})
+            return res.status(200).send({ message: 'category already use' })
             
         }
         const addCategory = await query(`INSERT INTO categories VALUES (null,${db.escape(name)},null)`)
-        return res.status(200).send({message:'category been added'})
+        return res.status(200).send({ message: 'category been added' })
         
-    }
+    },
 }
--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { productsController } = require('../controllers')
+const upload = require('../middleware/multer')
 
 const router = express.Router()
 
-router.post('/add-product', productsController.addProduct)
+router.post('/add-product',upload.single('file') ,productsController.addProduct)
 router.post('/add-category',productsController.addCategory)
 
 module.exports = router
